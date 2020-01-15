49a483d28f2896afd4dbc43d4178b28f8b3a5f52
vdegenne <vdegenne@gmail.com>
2020-01-15 22:32:31 +0100

    remove builds + fix front rollup

--- src/front/rollup.config.js
+++ src/front/rollup.config.js
@@ -1,4 +1,4 @@
-import nodeResolvePlugin from 'rollup-plugin-node-resolve';
+import nodeResolve from 'rollup-plugin-node-resolve';
 import {terser} from 'rollup-plugin-terser';
 import typescript from 'rollup-plugin-typescript';
 
